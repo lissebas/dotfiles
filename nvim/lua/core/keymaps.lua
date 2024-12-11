b4df8c980505ab5362af91ecff979399973d847a
lissebas <grsebas04@gmail.com>
2024-12-10 22:13:36 -0500

    base clipboard system integrated

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,4 +14,27 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
+
+
+-- Keymaps for clipboard copy/paste
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Copy to clipboard in visual mode
+keymap("v", "<C-c>", '"+y', opts)
+
+-- Paste from clipboard in normal mode
+keymap("n", "<C-p>", '"+p', opts)
+
+-- Paste from clipboard in visual mode (replace selected text)
+keymap("v", "<C-p>", '"+p', opts)
+
+-- Copy to clipboard in normal mode (optional, if needed)
+keymap("n", "<C-c>", '"+yy', opts)
+
+
+
+
+
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
