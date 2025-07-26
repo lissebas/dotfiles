bbf9cbdf5dd4c6584e0117c7e3ff92a63052d6f2
lissebas <grsebas04@gmail.com>
2025-07-26 12:13:58 -0500

    Backup ENV UI

--- scripts/ShowBrightness.sh
+++ scripts/ShowBrightness.sh
@@ -5,7 +5,7 @@ appName="VideoManager"
 brightness=$(brightnessctl -d amdgpu_bl1 | grep "Current brightness" | awk '{print $4}' | sed 's/[^0-9]*//g')
 
 # Show the brightness notification
-dunstify -a "$appName" -u low -i computer \
+dunstify -a "$appName" -u low \
   -h string:x-dunst-stack-tag:$msgTag \
   -h int:value:"$brightness" \
-  "${brightness}%"
+  "󰃠 ${brightness}%"
--- scripts/ShowVolume.sh
+++ scripts/ShowVolume.sh
@@ -5,19 +5,25 @@
 msgTag="myvolume"
 appName="SoundManager"
 
-# Query amixer for the current volume and whether or not the speaker is muted
-volume="$(amixer get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
-mute="$(amixer get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
+
+getVolume() {
+  amixer get Master | tail -1 | awk -F'[][]' '{ print $2 }' | sed 's/%//'
+}
+
+getStatus() {
+  amixer get Master | tail -1 | awk -F'[][]' '{ print $4 }'
+}
+
+volume=$(getVolume)
+mute=$(getStatus)
 
 if [[ $volume == 0 || "$mute" == "off" ]]; then
     # Show the sound muted notification
-    dunstify -a $appName -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
+    dunstify -a $appName -u low -h string:x-dunst-stack-tag:$msgTag "  Volume muted" 
 else
     # Show the volume notification
-    dunstify -a "$appName" -u low -i audio-speakers \
+    dunstify -a "$appName" -u low \
       -h string:x-dunst-stack-tag:$msgTag \
       -h int:value:"$volume" \
-      "${volume}%"
-
-
+      "  ${volume}%"
 fi
